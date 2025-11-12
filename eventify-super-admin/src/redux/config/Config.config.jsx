c49ecd2aa3ca016f5fd1bbc40bd1f29139ab5b51
zain100000 <muhammadzainulabideen292@gmail.com>
2025-11-12 20:36:27 +0500

    admin panel completed

--- eventify-super-admin/src/redux/config/Config.config.jsx
+++ eventify-super-admin/src/redux/config/Config.config.jsx
@@ -9,7 +9,8 @@
 
 const CONFIG = {
   /** Base URL for backend API requests */
-  BACKEND_API_URL: "http://localhost:8000/api",
+  // BACKEND_API_URL: "http://localhost:8000/api",
+  BACKEND_API_URL: "https://eventify-backend-tan.vercel.app/api",
 };
 
 export default CONFIG;
